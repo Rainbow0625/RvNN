e08d797186215bade3a869ee6ec6921720e7c346
Rainbow0625 <347601193@qq.com>
2018-10-16 10:09:13 +0800

    ++

--- RvNN.py
+++ RvNN.py
@@ -25,57 +25,70 @@ class RvNN:
 
     # not finished!!!!!!!!!!!!!!!!!!!!!!
     def load_data(self):
-        self.train_data = 0
+        self.train_data = []
         self.test_data = 0
 
-    def add_layer(self, node, activation_function=None):
+    # 输入参数，输出一整颗组装好的树结构的root节点
+    # not finished!!!!!!!!!!!!!!!!!!!!!!
+    def parse(self, data):
+        root = TreeNode(data)  # data includes embedding
+        return root
 
-        # define the placeholder for inputs to network
-        x_c1 = tf.placeholder(tf.float32, [None, self.dimension])
-        x_c2 = tf.placeholder(tf.float32, [None, self.dimension])
-        x = tf.concat([x_c1, x_c2], 0)
+    def add_layers(self, node, activation_function=tf.tanh):  # tf.sigmoid 待定
+        if not node.isLeaf:
+            self.add_layers(node.left)
 
-        # if node.isLeaf:
+            # define the placeholder for inputs to network
+            x_c1 = tf.placeholder(tf.float32, [None, self.dimension])
+            x_c2 = tf.placeholder(tf.float32, [None, self.dimension])
+            x = tf.concat([x_c1, x_c2], 0)
 
-        regularizion = tf.contrib.layers.l2_regularizer(0.0001)  # L2 regularizion
-        with tf.variable_scope('Composition', initializer=tf.random_normal_initializer,
-                               regularizer=regularizion):
-            weight = tf.get_variable('weight', shape=[self.dimension, self.dimension*2])
-            bias = tf.get_variable('bias', shape=[self.dimension, 1])
-        Wx_plus_b = tf.matmul(weight, input) + bias
-        if activation_function is None:
-            output = Wx_plus_b
-        else:
-            output = activation_function(Wx_plus_b)
-        return output
+            regularizion = tf.contrib.layers.l2_regularizer(0.0001)  # L2 regularizion
+            with tf.variable_scope('Composition', initializer=tf.random_normal_initializer,
+                                   regularizer=regularizion):
+                weight = tf.get_variable('weight', shape=[self.dimension, self.dimension * 2])
+                bias = tf.get_variable('bias', shape=[self.dimension, 1])
+            Wx_plus_b = tf.matmul(weight, x) + bias
+            if activation_function is None:
+                output = Wx_plus_b
+            else:
+                output = activation_function(Wx_plus_b)
+            return output
 
-    def run_iter(self):
+    # not finished!!!!!!!!!!!!!!!!!!!!!!
+    def loss(self, output, y):
+        loss = 0
+        return loss
+
+    def run_iter(self, verbose=True):
+        loss_history = []
         for i in range(len(self.train_data)):
             sess = tf.Session()
             init_op = tf.global_variables_initializer()
             sess.run(init_op)
 
             # every sample go in to train
-            self.train_data[i]
+            node = self.parse(self.train_data[i])
+
+            # add hidden layer with the length of rule
+            # for i in range(self.rule_length - 1):  # 调用几次这个函数？计算复合的
+            output = self.add_layers(node)
+
+            y = 0  # where is the "y" from?!!!!!!!!!!!!
+            loss = self.loss(output, y)  # how to calculate?!!!!!!!!!!!!
+            train_step = tf.train.AdagradOptimizer(0.1).minimize(loss)  # learning rate could be halved?
+            loss, _ = sess.run([loss, train_step])
+            loss_history.append(loss)
+            if verbose:
+                print("Train step: %d / %d, mean loss: %s" % (i, len(self.train_data), np.mean(loss_history)))
             pass
 
 
     def train(self):
         self.load_data()
 
-        # 要有一个数据结构装这棵树
-        embedding = 0  # wait to change!!
-        node = TreeNode(embedding)
-
-        # add hidden layer with the length of rule
-        for i in range(self.rule_length-1):  # 调用几次这个函数？计算复合的
-            layer = self.add_layer(node, tf.tanh)   # tf.sigmoid 待定
-
-        loss = 0  # how to calculate?
-        train_step = tf.train.AdagradOptimizer(0.1).minimize(loss)  # learning rate could be halved?
-
-
         for iter in range(self.training_iteration):
+            # every iteration is for all data
             self.run_iter()
             pass
 
@@ -85,6 +98,7 @@ def test_RvNN():
     # init(self, dim, rule_length, training_iteration)
     # batch_size
     model.init(100, 2, 1000)
+
 
 if __name__ == "__main__":
     test_RvNN()
